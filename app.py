477a333c1fcc15e247175b09b1fdd922dd6f19d9
C4NDYPUFF <omrcgo223@gmail.com>
2023-11-17 16:25:43 -0600

    Last commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st 
 import pandas as pd 
-import plotly_express as px
+import plotly.express as px
 from plotting import plot_asistencia, plot_categoria, plot_influencia,plot_registros_evento, plot_asistencia_registros, plot_pie_chart
 from data_processing import load_data, mapear_tipo, mapear_asistencia, renombrar_cargos, renombrar_influencia, renombrar_medio
 
-ITM = load_data(st.secrets['EXCEL_FILE_PATH'])
+ITM = load_data(st.secrets('EXCEL_FILE_PATH'))
 ITM['Asistencia'] = ITM['Asistencia'].apply(mapear_asistencia)
 ITM = mapear_tipo(ITM)
 ITM['Cargo'] = ITM['Cargo'].apply(renombrar_cargos)
@@ -12,7 +12,7 @@ ITM['Nivel de influencia'] = ITM['Nivel de influencia'].apply(renombrar_influenc
 ITM['¿Cómo se enteró del evento?'] = ITM['¿Cómo se enteró del evento?'].apply(renombrar_medio)
 
 
-def main_app():
+def main_app(df):
     st.set_page_config(page_title='ITM DASHBOARD', page_icon=':bar_chart:', layout='wide')
     # Main page layout
     st.title(':chart_with_upwards_trend: ITM VISITANTES 2019-2023')
@@ -42,6 +42,8 @@ def main_app():
     fig_evento = plot_registros_evento(ITM)
     st.plotly_chart(fig_evento, use_container_width=True)
 
+    df = df.drop_duplicates(subset='Email', keep='last')
+
     fig_registros_asistencia = plot_asistencia_registros(ITM)
     st.plotly_chart(fig_registros_asistencia, use_container_width=True)
 
@@ -64,4 +66,4 @@ def main_app():
     st.plotly_chart(fig_2023_medio, use_container_width=True)
 
 
-main_app()
+main_app(ITM)
